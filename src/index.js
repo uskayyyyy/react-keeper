7a6def6ce0cc760f6f23366375acbedb4beac075
Yusuke Ito <u.skayyyyy@gmail.com>
2022-11-09 13:44:46 +0900

    Project Part 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Keeper App</h1>, root);
+root.render(<App />, root);
